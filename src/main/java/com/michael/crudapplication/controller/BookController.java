f68c335a57d7f228265b728c751fafdd685d7428
michael <maroc.michael@hotmail.com>
2024-05-01 23:23:17 +0200

    update book controller

--- src/main/java/com/michael/crudapplication/controller/BookController.java
+++ src/main/java/com/michael/crudapplication/controller/BookController.java
@@ -1,2 +1,25 @@
-package com.michael.crudapplication.controller;public class BookController {
+package com.michael.crudapplication.controller;
+
+import com.michael.crudapplication.model.Book;
+import com.michael.crudapplication.service.BookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/books")
+public class BookController {
+
+    private BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("")
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
 }
