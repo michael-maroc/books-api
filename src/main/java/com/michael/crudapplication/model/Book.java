9c42f503d4c697de851415b192ae7967e487bd26
michael <maroc.michael@hotmail.com>
2024-05-01 23:00:40 +0200

    add book model

---
+++ src/main/java/com/michael/crudapplication/model/Book.java
@@ -0,0 +1,59 @@
+package com.michael.crudapplication.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String author;
+
+    private String title;
+
+    private String type;
+
+    public Book(int id, String author, String title, String type) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
